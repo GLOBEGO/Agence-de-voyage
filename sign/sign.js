d191cad7fa14b76e28212a2909b050e94bdc96b9
Wael Ghouma <waelghouma28@gmail.com>
2024-04-20 17:19:54 +0100

    html js

---
+++ sign/sign.js
@@ -0,0 +1,12 @@
+function check() {
+    const mot = document.getElementById("password").value;
+    const confmot = document.getElementById("conf").value;
+    var msg = document.getElementById("msg");
+
+    if(confmot != mot){
+        msg.textContent = "mot de passe incompatible";
+        msg.style.backgroundColor = "#ff4500";
+        msg.style.color = "#0000";
+        
+    }
+}
